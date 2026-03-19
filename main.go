a51f8b2b7e45ee305a13decb7b8821e488bd34ae
Glenn-Rhee <glennviktor5@gmail.com>
2026-03-19 15:29:09 +0700

    feat: added three file

---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"log"
+	"notes_app/src/controller"
+	"notes_app/src/lib"
+	"notes_app/src/model"
+	"notes_app/src/service"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main(){
+	db, err := lib.DbConnect()
+	if err != nil {
+		log.Fatalf("Failed to connect DB: %v\n", err)
+	}
+
+	db.AutoMigrate(&model.Note{})
+
+	noteService := service.NewNoteService(db)
+	noteController := controller.NewNoteService(noteService)
+
+	router := gin.Default()
+
+	router.GET("/notes", noteController.NotesGet)
+
+	if err := router.Run(); err != nil {
+		log.Fatalf("Failed to run server: %v\n", err)
+	}
+}
