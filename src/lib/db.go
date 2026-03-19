3ffc53c31322386dc62b49f2f09822b92322ac4a
Glenn-Rhee <glennviktor5@gmail.com>
2026-03-19 15:29:42 +0700

    feat: added lib db

---
+++ src/lib/db.go
@@ -0,0 +1,17 @@
+package lib
+
+import (
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+func DbConnect() (*gorm.DB, error) {
+	dsn := "root:@tcp(127.0.0.1:3306)/notes_app_flutter?charset=utf8mb4&parseTime=True&loc=Local"
+
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		return  nil, err
+	}
+	
+	return db, nil
+}
