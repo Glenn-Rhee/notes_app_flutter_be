4d7610129eb01c83c43ee640d7b05c63d56395f9
Glenn-Rhee <glennviktor5@gmail.com>
2026-03-20 04:13:32 +0700

    feat: added service for create note

--- src/service/notes-service.go
+++ src/service/notes-service.go
@@ -1,9 +1,10 @@
 package service
 
 import (
-	"net/http"
 	"notes_app/src/model"
+	"time"
 
+	"github.com/google/uuid"
 	"gorm.io/gorm"
 )
 
@@ -15,14 +16,38 @@ func NewNoteService(db *gorm.DB) *NoteService{
 	return &NoteService{DB: db}
 }
 
-func (s *NoteService) GetNotes() (res model.ResponsePayload, err error)  {
+func (s *NoteService) GetNotes() (res []model.Note, err error)  {
 	var notes []model.Note
 
 	err = s.DB.Find(&notes).Error
-	return model.ResponsePayload{
-		Code: http.StatusOK,
-		Status: "Success",
-		Message: "Successfully get data",
-		Data: notes,
+	if err != nil {
+		return nil, err
+	}
+
+	return notes, nil
+}
+
+func (s *NoteService) CreateNotes(dataNote model.CreateNote) (res model.NoteResponse, err error){
+	id := uint(uuid.New().ID())
+
+	note := model.Note{
+		ID: id,
+		Title: dataNote.Title,
+		Content: dataNote.Content,
+		CreatedAt: time.Now(),
+		UpdatedAt: time.Now(),
+	}
+
+	result := s.DB.Create(&note)
+
+	if result.Error != nil {
+		return model.NoteResponse{}, result.Error
+	}
+
+	return model.NoteResponse{
+		ID: note.ID,
+		Title: note.Title,
+		Content: note.Content,
+		CreatedAt: note.CreatedAt,
 	}, nil
 }
