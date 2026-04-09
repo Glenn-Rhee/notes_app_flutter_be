3c1ca6d8229106e4968c9078442ed873c90dc101
Glenn-Rhee <glennviktor5@gmail.com>
2026-04-09 18:54:46 +0700

    refactor: update db.go

--- src/lib/db.go
+++ src/lib/db.go
@@ -9,6 +9,7 @@ func DbConnect() (*gorm.DB, error) {
 	// DSN (Data Source Name) untuk koneksi ke database MySQL
 	dsn := "root:@tcp(127.0.0.1:3306)/notes_app_flutter?charset=utf8mb4&parseTime=True&loc=Local"
 
+	// Membuka koneksi ke database menggunakan GORM dengan driver MySQL
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		return  nil, err
